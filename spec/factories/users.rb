0880e741cc8fc71a933fd21e5e503c37d9676f4e
yuki-MIRA <oyuuki0423@gmail.com>
2020-11-25 19:12:55 +0900

    FactoryBotファイルの修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname               { Faker::Name.initials(number: 2) }
     email                  { Faker::Internet.free_email }
-    password               { Faker::Internet.password(min_length: 6) }
+    password               { "aaa111" }
     password_confirmation  { password }
     family_name            { '田中' }
     first_name             { '太郎' }
