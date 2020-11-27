4500ac9b948fb8401393662483f2738cb03b2066
yuki-MIRA <oyuuki0423@gmail.com>
2020-11-27 22:05:27 +0900

    テストコード実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
+    association :user
+
+    name              {'テスト出品'}
+    explanatory_text  {'テスト説明'}
+    price             {1000}
+    status_id         {1}
+    delivery_fee_id   {1}
+    address_id        {1}
+    delivery_date_id  {1}
+    genre_id          {1}
     
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/sample1.png'), filename: 'sample1.png')
+    end
   end
 end
