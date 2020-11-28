727eb9f76fb0c89602c3ffb0e481a2f45103aa4e
yuki-MIRA <oyuuki0423@gmail.com>
2020-11-28 11:27:40 +0900

    itemモデル、バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,13 +6,17 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :explanatory_text
-    validates :price, numericality: { only_integer: true, message: 'is invalid. Input half-width characters.' }
-    validates :status_id, numericality: { other_than: 0, message: 'Select' }
-    validates :delivery_fee_id, numericality: { other_than: 0, message: 'Select' }
-    validates :address_id, numericality: { other_than: 0, message: 'Select' }
-    validates :delivery_date_id, numericality: { other_than: 0, message: 'Select' }
-    validates :genre_id, numericality: { other_than: 0, message: 'Select' }
     validates :image
+    validates :price, numericality: { only_integer: true, message: 'is invalid. Input half-width characters.' }
   end
+
+  with_options numericality: { other_than: 0, message: 'Select' } do
+    validates :status_id
+    validates :delivery_fee_id
+    validates :address_id
+    validates :delivery_date_id
+    validates :genre_id
+  end
+  
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is out of setting range' }
 end
