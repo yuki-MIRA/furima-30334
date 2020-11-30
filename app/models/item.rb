06760360188f0f0c5b5f7a8bac31045e601a0978
yuki-MIRA <oyuuki0423@gmail.com>
2020-11-30 21:02:01 +0900

    rubocop実行

--- app/models/item.rb
+++ app/models/item.rb
@@ -19,7 +19,7 @@ class Item < ApplicationRecord
   end
 
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is out of setting range' }
-  
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :delivery_fee
   belongs_to :genre
