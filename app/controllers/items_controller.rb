53abc3df4f4e143cae60439027127b645484a970
yuki-MIRA <oyuuki0423@gmail.com>
2020-12-02 21:44:45 +0900

    destroyアクション実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,6 +34,14 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    item = Item.find(params[:id])
+    if current_user.id == item.user_id
+      item.destroy
+      redirect_to root_path
+    end
+  end
+
   private
 
   def item_params
