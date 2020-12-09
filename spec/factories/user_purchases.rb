785a09ea033ec569f3e88c38d08d4ba74960e932
yuki-MIRA <oyuuki0423@gmail.com>
2020-12-09 18:31:32 +0900

    テストコード実装【クレジットカード情報登録以外】

---
+++ spec/factories/user_purchases.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user_purchase do
+    postal_code  { '123-4567' }
+    address_id   { 1 }
+    city         { '札幌市' }
+    house_number { 1-1-1 }
+    tell_number  { '09012345678' }
+  end
+end
