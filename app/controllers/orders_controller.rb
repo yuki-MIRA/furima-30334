69f443a879ff00bfe1a400b1cd0808baa9ef48fb
yuki-MIRA <oyuuki0423@gmail.com>
2020-12-11 19:30:43 +0900

    コントローラー、カード情報保存、ページ遷移制限の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,6 @@
 class OrdersController < ApplicationController
   before_action :set_item, only: [:index, :create]
+  before_action :move_to_rogin, :move_to_top, :other_move_to_top, only: :index
 
   def index
     @user_purchase = UserPurchase.new
@@ -8,6 +9,7 @@ class OrdersController < ApplicationController
   def create
     @user_purchase = UserPurchase.new(user_purchase_params)
     if @user_purchase.valid?
+      pay_item
       @user_purchase.save
       redirect_to root_path
     else
@@ -18,11 +20,38 @@ class OrdersController < ApplicationController
   private
 
   def user_purchase_params
-    params.require(:user_purchase).permit(:postal_code, :address_id, :city, :house_number, :building_name, :tell_number).merge(user_id: current_user.id, item_id: @item.id) 
+    params.require(:user_purchase).permit(:postal_code, :address_id, :city, :house_number, :building_name, :tell_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token]) 
   end
 
   def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: user_purchase_params[:token],
+        currency: 'jpy'
+      )
+  end
+
+  def move_to_rogin
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
+
+  def move_to_top
+    if current_user.id == @item.user_id
+      redirect_to root_path
+    end
+  end
+
+  def other_move_to_top
+    unless @item.purchase_record == nil
+      redirect_to root_path
+    end
   end
 
 end
