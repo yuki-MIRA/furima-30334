945cc2f4fcdd46f88668adaf9589959367026fab
yuki-MIRA <oyuuki0423@gmail.com>
2020-12-11 21:01:26 +0900

    factoryBotファイル修正

--- spec/factories/user_purchases.rb
+++ spec/factories/user_purchases.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     postal_code  { '123-4567' }
     address_id   { 1 }
     city         { '札幌市' }
-    house_number { 1 - 1 - 1 }
+    house_number { '1 - 1 - 1' }
     tell_number  { '09012345678' }
     token        { 'tok_abcdefghijk00000000000000000' }
   end
