a7b770b171786d1f33a20c30485709c66bacdbf8
yuki-MIRA <oyuuki0423@gmail.com>
2020-12-11 19:28:48 +0900

    カード情報の値取得。バリデーション設定

--- app/models/user_purchase.rb
+++ app/models/user_purchase.rb
@@ -1,6 +1,6 @@
 class UserPurchase
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :address_id, :city, :house_number, :building_name, :tell_number, :purchase_record
+  attr_accessor :user_id, :item_id, :postal_code, :address_id, :city, :house_number, :building_name, :tell_number, :purchase_record, :token
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
@@ -8,11 +8,13 @@ class UserPurchase
     validates :city
     validates :house_number
     validates :tell_number, format: {with: /\A\d{11}\z/, message: "input Only Numbers.Number of digits 11"}
+    validates :token
   end
 
   def save
     purchase_record = PurchaseRecord.create(user_id: user_id, item_id: item_id)
     BuyerAddress.create(postal_code: postal_code, address_id: address_id, city: city, house_number: house_number, building_name: building_name, tell_number: tell_number, purchase_record_id: purchase_record.id)
   end
+
 end
 
